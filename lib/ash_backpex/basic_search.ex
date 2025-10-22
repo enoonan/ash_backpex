f07b96e5abc4bc1065212948723fde12e3f03a66
Eileen Noonan <enoonan@arcstone.com>
2025-10-22 10:44:33 -0400

    chore: add typespec and docs

--- lib/ash_backpex/basic_search.ex
+++ lib/ash_backpex/basic_search.ex
@@ -1,7 +1,24 @@
 defmodule AshBackpex.BasicSearch do
+  @moduledoc """
+  Supports basic search operations using the Ash.Expr `contains` function.
+  """
+  alias ElixirLS.LanguageServer.Providers.Completion.Reducers.Returns
   require Ash.Query
 
-  @spec apply(Ash.Query.t(), map(), module()) :: Ash.Query.t()
+  @type query :: Ash.Query.t()
+  @type params :: map()
+  @type live_resource :: module()
+
+  @doc """
+    Takes a query along, a map representing the `params` key of an `assigns` map, and a live_resource.
+    Returns the query with search filters applied.
+    If no search value is supplied, or the given `live_resource` doesn't contain any searchable fields, the query is returned unmodified.
+
+    ```elixir
+      AshBackpex.BasicSearch.apply(query, %{"search" => "foo"}, MyLiveResource)
+    ```
+  """
+  @spec apply(query(), params(), live_resource()) :: Ash.Query.t()
   def apply(query, %{"search" => search}, live_resource) do
     case live_resource.fields() |> Enum.filter(&Map.get(elem(&1, 1), :searchable)) do
       [] ->
